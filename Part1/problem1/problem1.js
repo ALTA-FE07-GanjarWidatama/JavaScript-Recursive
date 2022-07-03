2e73d50ad93076148df19eb85e48e0a8ad818f1f
Ganjar Widatama <gwidatama@gmail.com>
2022-07-03 22:47:20 +0700

    tugas

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -1,5 +1,11 @@
-function primeX(number) {
-  // Your code here
+function primeX(num) {
+  let arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+  let reveal = arr[num - 1];
+  return reveal;
 }
 
-module.exports = primeX;
+console.log(primeX(1));
+console.log(primeX(5));
+console.log(primeX(8));
+console.log(primeX(9));
+console.log(primeX(10));
--- Part1/problem2/problem2.js
+++ Part1/problem2/problem2.js
@@ -1,5 +1,13 @@
-function fibonacci(number) {
-  // Your code here
+function fibonacci(num) {
+  if (num <= 1) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
-module.exports = fibonacci;
+console.log(fibonacci(0));
+console.log(fibonacci(2));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
+console.log(fibonacci(12));
